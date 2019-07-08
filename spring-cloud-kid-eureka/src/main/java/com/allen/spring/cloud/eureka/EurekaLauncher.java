14f706745e3cec060e2625d93b8443999a5381c3
wanglun <wanglun@yunnex.com>
2019-07-08 16:33:37 +0800

    '优化Eureka'

--- spring-cloud-kid-eureka/src/main/java/com/allen/spring/cloud/eureka/EurekaLauncher.java
+++ spring-cloud-kid-eureka/src/main/java/com/allen/spring/cloud/eureka/EurekaLauncher.java
@@ -2,18 +2,11 @@ package com.allen.spring.cloud.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class EurekaLauncher extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(EurekaLauncher.class);
-    }
+public class EurekaLauncher {
 
     public static void main(String...args) {
         SpringApplication.run(EurekaLauncher.class, args);
---
+++ spring-cloud-kid-gateway/src/main/java/com/allen/spring/cloud/gateway/controller/GatewayFallbackController.java
@@ -0,0 +1,43 @@
+package com.allen.spring.cloud.producer.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class HelloController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping("/hello")
+    public String hello(@RequestParam("name") String name) {
+        LOGGER.info("request parameter is : " + name);
+        try {
+            Thread.sleep(300000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "Hello " + name + ", this is the first message.";
+    }
+
+    @RequestMapping("/service_info")
+    public List<String> serviceInfo() {
+        return discoveryClient.getServices();
+    }
+
+    @RequestMapping("/show_owner")
+    public String showOwner(@RequestParam(value = "owner", required = false) String owner) {
+        LOGGER.info("The owner is : " + owner);
+        return "The owner is : " + owner;
+    }
+
+}
