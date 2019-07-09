76899d0715feba4c0192a6125409b8e43dd142fc
wanglun <wanglun@yunnex.com>
2019-07-09 16:08:13 +0800

    '增加@EnableEurekaClient和@EnbaleDiscoveryClient的比较'

--- spring-cloud-kid-zuul/src/main/java/com/allen/spring/cloud/zuul/ZuulLauncher.java
+++ spring-cloud-kid-zuul/src/main/java/com/allen/spring/cloud/zuul/ZuulLauncher.java
@@ -6,6 +6,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
+// spring cloud中discovery service有许多种实现（eureka、consul、zookeeper等等），@EnableDiscoveryClient基于spring-cloud-commons, @EnableEurekaClient基于spring-cloud-netflix。
+// 如果选用的注册中心是eureka，那么就推荐@EnableEurekaClient
+// 如果是其他的注册中心，那么推荐使用@EnableDiscoveryClient
 @EnableEurekaClient
 @SpringBootApplication
 public class ZuulLauncher {
