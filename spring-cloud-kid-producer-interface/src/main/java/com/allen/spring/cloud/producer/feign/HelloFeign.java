e8c8a113055118b9447fee15e025d8c793c3c7d1
wanglun <wanglun@yunnex.com>
2019-10-24 19:24:19 +0800

    整理代码

--- spring-cloud-kid-producer-interface/src/main/java/com/allen/spring/cloud/producer/feign/HelloFeign.java
+++ spring-cloud-kid-producer-interface/src/main/java/com/allen/spring/cloud/producer/feign/HelloFeign.java
@@ -1,8 +1,7 @@
-package com.allen.spring.cloud.consumer.feign;
+package com.allen.cloud.producer.feign;
 
-import com.allen.spring.cloud.consumer.hystrix.HelloHystrix;
+import com.allen.cloud.producer.hystrix.HelloHystrix;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -11,13 +10,18 @@ import java.util.List;
 /**
  * @FeignClient 需要指向的是服务提供者
  */
-@Component
+//@Component
+//name:远程服务名，即：spring.application.name配置的名称
 @FeignClient(name = "spring-cloud-kid-producer", fallback = HelloHystrix.class)
 public interface HelloFeign {
 
+    //方法名和参数必须与远程服务中contoller中的保持一致。
     @RequestMapping("/hello")
-    String hello(@RequestParam("name") String name);
+    String hello(@RequestParam(value = "name", required = false) String name);
 
     @RequestMapping("/service_info")
     List<String> serviceInfo();
+
+    @RequestMapping("/show_owner")
+    String showOwner(@RequestParam(value = "owner", required = false) String owner);
 }
--- spring-cloud-kid-producer-interface/src/main/java/com/allen/spring/cloud/producer/hystrix/HelloHystrix.java
+++ spring-cloud-kid-producer-interface/src/main/java/com/allen/spring/cloud/producer/hystrix/HelloHystrix.java
@@ -1,8 +1,7 @@
-package com.allen.spring.cloud.consumer.hystrix;
+package com.allen.cloud.producer.hystrix;
 
-import com.allen.spring.cloud.consumer.feign.HelloFeign;
+import com.allen.cloud.producer.feign.HelloFeign;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,12 +10,17 @@ import java.util.List;
 public class HelloHystrix implements HelloFeign {
 
     @Override
-    public String hello(@RequestParam("name") String name) {
+    public String hello(String name) {
         return "Hello " + name + ", this is a hystrix message.";
     }
 
     @Override
     public List<String> serviceInfo() {
         return new ArrayList<>();
+    }
+
+    @Override
+    public String showOwner(String owner) {
+        return "This is a hystrix message.";
     }
 }
