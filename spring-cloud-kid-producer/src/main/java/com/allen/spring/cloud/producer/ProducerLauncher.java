0e160458694c91a2f7f9a80a64570efddfe6cdb3
wanglun <wanglun@yunnex.com>
2019-10-25 15:44:21 +0800

    EnableDiscoveryClient改为EnableEurekaClient

--- spring-cloud-kid-producer/src/main/java/com/allen/spring/cloud/producer/ProducerLauncher.java
+++ spring-cloud-kid-producer/src/main/java/com/allen/spring/cloud/producer/ProducerLauncher.java
@@ -2,10 +2,10 @@ package com.allen.spring.cloud.producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 //启动服务注册与发现
-@EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
 public class ProducerLauncher {
 
