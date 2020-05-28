87e90db62607785918992a466d818926f8d84c7a
wanglun <wanglun@yunnex.com>
2020-05-28 17:23:02 +0800

    增加Hystrix Dashboard

--- spring-cloud-kid-consumer/src/main/java/com/allen/spring/cloud/consumer/ConsumerLauncher.java
+++ spring-cloud-kid-consumer/src/main/java/com/allen/spring/cloud/consumer/ConsumerLauncher.java
@@ -4,12 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
 
-//@EnableHystrixDashboard
+@EnableHystrixDashboard
 /**
  * 启用服务注册与发现
  * Spring Cloud中服务注册与发现有许多种实现（eureka、consul、zookeeper等等），
