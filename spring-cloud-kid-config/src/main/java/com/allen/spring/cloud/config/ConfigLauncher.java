3692778262251473336e2e709a4c5cfeceed478b
wanglun <wanglun@yunnex.com>
2020-05-29 10:48:13 +0800

    Spring cloud config server

--- spring-cloud-kid-config/src/main/java/com/allen/spring/cloud/config/ConfigLauncher.java
+++ spring-cloud-kid-config/src/main/java/com/allen/spring/cloud/config/ConfigLauncher.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+// 激活对配置中心的支持
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigLauncher {
