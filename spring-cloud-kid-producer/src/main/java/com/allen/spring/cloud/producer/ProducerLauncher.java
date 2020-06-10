06f55eb951fc353f8d7eaaa6d16c9a8e509b845d
wanglun <wanglun@yunnex.com>
2020-06-10 16:39:51 +0800

    使用Nacos注册中心

--- spring-cloud-kid-producer/src/main/java/com/allen/spring/cloud/producer/ProducerLauncher.java
+++ spring-cloud-kid-producer/src/main/java/com/allen/spring/cloud/producer/ProducerLauncher.java
@@ -2,10 +2,12 @@ package com.allen.spring.cloud.producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 //启动服务注册与发现
 @EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ProducerLauncher {
 
