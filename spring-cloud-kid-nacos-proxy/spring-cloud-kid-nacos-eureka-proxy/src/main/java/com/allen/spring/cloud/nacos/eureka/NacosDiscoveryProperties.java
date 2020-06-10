e07e4a65f0eb85f7ec5356e3085475436a926c6d
wanglun <wanglun@yunnex.com>
2020-06-10 15:19:44 +0800

    添加Eureka迁移到Nacos的同步Proxy

--- spring-cloud-kid-nacos-proxy/spring-cloud-kid-nacos-eureka-proxy/src/main/java/com/allen/spring/cloud/nacos/eureka/NacosDiscoveryProperties.java
+++ spring-cloud-kid-nacos-proxy/spring-cloud-kid-nacos-eureka-proxy/src/main/java/com/allen/spring/cloud/nacos/eureka/NacosDiscoveryProperties.java
@@ -1,9 +1,26 @@
 package com.allen.spring.cloud.nacos.eureka;
 
+import com.alibaba.nacos.api.PropertyKeyConst;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.core.env.Environment;
+import org.springframework.util.StringUtils;
 
+import javax.annotation.PostConstruct;
 import java.util.Properties;
 
 @ConfigurationProperties("spring.cloud.nacos.discovery")
 public class NacosDiscoveryProperties extends Properties {
+
+    @Autowired
+    private Environment environment;
+
+    @PostConstruct
+    public void init() {
+        String serverAddr = getProperty(PropertyKeyConst.SERVER_ADDR);
+        if (StringUtils.isEmpty(serverAddr)) {
+            setProperty(PropertyKeyConst.SERVER_ADDR, environment
+                    .resolvePlaceholders("${spring.cloud.nacos.discovery.server-addr:}"));
+        }
+    }
 }
